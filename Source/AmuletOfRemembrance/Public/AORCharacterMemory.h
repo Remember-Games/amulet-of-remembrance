6e4b6c9d335724fd370647b1e7bdabe8555b7493
EUDALD ELIAS VILANOVA <eudald.elias@gmail.com>
2022-05-02 22:45:50 +0200

    Base classes for character

---
+++ Source/AmuletOfRemembrance/Public/AORCharacterMemory.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class AMULETOFREMEMBRANCE_API AORCharacterMemory
+{
+public:
+	AORCharacterMemory();
+	~AORCharacterMemory();
+};
---
+++ Source/AmuletOfRemembrance/Public/AORShadowController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AORShadowController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AMULETOFREMEMBRANCE_API AAORShadowController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
